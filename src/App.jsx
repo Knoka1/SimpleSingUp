0e00a6ce2cf45f42c69998cdd8d486c9a435a5bc
Knoka1 <knoka1@hotmail.com>
2023-06-19 18:53:08 -0300

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { Tooltip, Toast, Popover } from "bootstrap";
+import * as bootstrap from "bootstrap";
+import Alert from "bootstrap/js/dist/alert";
+
+import "./App.css";
+import "./scss/styles.scss";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+          <small id="emailHelp" class="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <div class="form-group form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+          <label class="form-check-label" for="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default App;
