2d790fa3538091fce905c4db6da11882b69460d9
Knoka1 <knoka1@hotmail.com>
2023-06-20 14:33:54 -0300

    feature: Added responsiveness and similar design

--- src/App.jsx
+++ src/App.jsx
@@ -13,38 +13,58 @@ function App() {
   const [date, setDate] = useState(new Date());
 
   return (
-    <div className="signup template d-flex justify-content-center align-items-center vh-100 bg-primary">
-      <div className="form_container p-5 rounded bg-white">
+    <div className="signup template d-flex justify-content-center align-items-center vh-100">
+      <div className="form_container p-5 rounded">
         <form>
-          <h3 className="text-center">Sign Up</h3>
-          <div className="mb-2">
-            <label htmlForm="email">Email</label>
-            <input
-              type="email"
-              placeholder="Enter Email"
-              className="form-control"
-            />
-          </div>
-          <div className="mb-2">
-            <label htmlForm="password">Password</label>
-            <input
-              type="password"
-              placeholder="Enter Password"
-              className="form-control"
-            />
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              className="custom-control custom-checkbox"
-              id="check"
-            />
-            <label htmlFor="check" className="custom-input-label ms-2">
-              Remeber me
-            </label>
-          </div>
-          <div className="d-grid">
-            <button className="btn btn-primary">Submit</button>
+          <div className="container box">
+            <div className="">
+              <h3 className="text-left">S'inscrire</h3>
+              <h6 className="subText text-left">C'est rapide et facile</h6>
+            </div>
+            <div className="mb-2 row">
+              <div className="col">
+                <input
+                  type="text"
+                  placeholder="Prénom"
+                  className="form-control"
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="nom"
+                  placeholder="Nom de famille"
+                  className="form-control"
+                />
+              </div>
+            </div>
+            <div className="mb-2">
+              <label htmlFor="Email">Email</label>
+              <input
+                type="email"
+                placeholder="Número de mobille ou e-mail"
+                className="form-control"
+              />
+            </div>
+            <div className="mb-2">
+              <label htmlFor="naissance">Date de naissance</label>
+              <DatePicker
+                className="form-control"
+                selected={date}
+                onChange={(date) => setDate(date)}
+              />
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="custom-control custom-checkbox"
+                id="check"
+              />
+              <label htmlFor="check">Remeber me</label>
+            </div>
+
+            <div className="d-grid">
+              <button className="btn btn-primary">Submit</button>
+            </div>
           </div>
         </form>
       </div>
