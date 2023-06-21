6fa41876f7219cb1d4e5609dc67f28e0b5056d35
Knoka1 <knoka1@hotmail.com>
2023-06-21 14:56:57 -0300

    feature: Added a reasonable sketch for List of Users card

--- src/components/RegisteredUser.jsx
+++ src/components/RegisteredUser.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "../scss/styles.scss";
-
+import "./RegisteredUser.css";
 const RegisteredUser = () => {
   return (
-    <div className=" justify-content-center align-items-center vh-10 ">
-      <div className="p-5 box rounded ">
-        <h3>Users:</h3>
-        <div>
-          <img />
-          <h5>User a</h5>
+    <div className="card mb-3 card_custom container">
+      <h3>List of Users: </h3>
+      <div className="row no-gutters">
+        <div className="col-md-2">
+          <img
+            src="https://cdn.britannica.com/99/236599-050-1199AD2C/Mark-Zuckerberg-2019.jpg"
+            className="card-img card_img_custom "
+            alt="Registered user"
+          />
+        </div>
+
+        <div className="col-md-10">
+          <div className="card-body">
+            <h6 className="card-title">Mark Zuckerberg</h6>
+            <p className="card-text">
+              <small>Homme 11/01/1990</small>
+            </p>
+          </div>
         </div>
       </div>
     </div>
