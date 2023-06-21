39d5218e1c8baf69f7140063b49dfaaf21a18e0c
Knoka1 <knoka1@hotmail.com>
2023-06-20 21:20:49 -0300

    feature: Adding new RegisteredUser component

---
+++ src/components/RegisteredUsers.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../scss/styles.scss";
+
+const RegisteredUsers = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-10 pr- ">
+      <div className="p-5 box rounded ">
+        <h3>Users:</h3>
+        <div>
+          <img />
+          <h5>User a</h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegisteredUsers;
