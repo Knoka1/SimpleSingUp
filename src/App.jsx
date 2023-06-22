02bb2494a255b21381e126301d412c90419424f1
Knoka1 <knoka1@hotmail.com>
2023-06-21 21:33:51 -0300

    fix: Added resposiveness for RegisteredUser component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SignUp from "./components/SignUp";
 import RegisteredUser from "./components/RegisteredUser";
 
@@ -7,15 +7,38 @@ import "./scss/styles.scss";
 import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, []);
+
+  const updateMedia = () => {
+    setIsMobile(window.innerWidth <= 600);
+  };
   return (
-    <div className="bg-color d-flex">
-      <div className=" bg-color2">
-        <SignUp />
-      </div>
-      <div className="d-flex bg-color3 justify-content-center align-items-center">
-        <RegisteredUser />
-      </div>
-    </div>
+    <>
+      {isMobile ? (
+        <div className="bg-color d-flex-inline">
+          <div className=" bg-color2">
+            <SignUp />
+          </div>
+          <div className="d-flex bg-color3 justify-content-center align-items-center">
+            <RegisteredUser />
+          </div>
+        </div>
+      ) : (
+        <div className="bg-color d-flex">
+          <div className=" bg-color2">
+            <SignUp />
+          </div>
+          <div className="d-flex bg-color3 justify-content-center align-items-center">
+            <RegisteredUser />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
